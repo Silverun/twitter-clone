3dbb342967e3feda46a149fb67b974761d76f5a2
silverun <imonutiy@gmail.com>
2023-04-04 22:27:09 +0300

    Auth UI add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import Layout from "@/components/Layout";
+import LoginModal from "@/components/Modals/LoginModal";
+import RegisterModal from "@/components/Modals/RegisterModal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <RegisterModal />
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
